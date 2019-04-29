c6017fc58bfa7e3fd131a52e6e2aca810bee8190
g0tmi1k <have.you.g0tmi1k@gmail.com>
2019-04-29 12:53:53 +0100

    Bash clean up

--- build.sh
+++ build.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-# Install dependencies (debbootstrap)
-sudo apt-get install -yqq debootstrap curl
-
 # -----------------------------------------------------------------------------
 # BUILD/LABEL VARIABLES
 # -----------------------------------------------------------------------------
-BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+BUILD_DATE=$( date -u +"%Y-%m-%dT%H:%M:%SZ" )
 VERSION="latest"
-VCS_URL=$(git config --get remote.origin.url)
-VCS_REF=$(git rev-parse --short HEAD)
+VCS_URL=$( git config --get remote.origin.url )
+VCS_REF=$( git rev-parse --short HEAD )
+
+# Install dependencies (debbootstrap)
+sudo apt-get install -yqq debootstrap curl
 
 # Fetch the latest Kali debootstrap script from git
 curl "https://gitlab.com/kalilinux/packages/debootstrap/raw/kali/master/scripts/kali" > kali-debootstrap && \
-sudo debootstrap kali-rolling ./kali-root https://http.kali.org/kali ./kali-debootstrap &&\
-sudo tar -C kali-root -c . | sudo docker import - kalilinux/kali-linux-docker &&\
-sudo rm -rf ./kali-root &&\
-TAG=$(sudo docker run -t -i kalilinux/kali-linux-docker awk '{print $NF}' /etc/debian_version | sed 's/\r$//' ) &&\
-echo "Tagging kali with $TAG" &&\
-sudo docker tag kalilinux/kali-linux-docker:$VERSION kalilinux/kali-linux-docker:$TAG &&\
-echo "Labeling kali" &&\
-sudo docker build --squash --rm -t kalilinux/kali-linux-docker:$VERSION \
---build-arg BUILD_DATE=$BUILD_DATE \
---build-arg VERSION=$VERSION \
---build-arg VCS_URL=$VCS_URL \
---build-arg VCS_REF=$VCS_REF . &&\
-echo "Build OK" || echo "Build failed!"
+  sudo debootstrap kali-rolling ./kali-root https://http.kali.org/kali ./kali-debootstrap && \
+  sudo tar -C kali-root -c . | sudo docker import - kalilinux/kali-linux-docker && \
+  sudo rm -rf ./kali-root && \
+  TAG=$( sudo docker run -t -i kalilinux/kali-linux-docker awk '{print $NF}' /etc/debian_version | sed 's/\r$//' ) && \
+  echo "Tagging kali with $TAG" && \
+  sudo docker tag kalilinux/kali-linux-docker:$VERSION kalilinux/kali-linux-docker:$TAG && \
+  echo "Labeling kali" && \
+  sudo docker build \
+    --squash \
+    --rm \
+    -t kalilinux/kali-linux-docker:$VERSION \
+    --build-arg BUILD_DATE=$BUILD_DATE \
+    --build-arg VERSION=$VERSION \
+    --build-arg VCS_URL=$VCS_URL \
+    --build-arg VCS_REF=$VCS_REF .
+  && echo "Build OK" \
+  || echo "Build failed!"
