db1845884491a5b53b8e37ce4f50f63cd06120ec
Arnaud Rebillout <arnaudr@kali.org>
2021-03-23 15:32:59 +0700

    build-rootfs: Rename work_dir into rootfsDir
    It's not about taste, I'm trying to minimize the changes that were
    merged in MR !30, so that in the end we can see what exactly was
    changed.

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -6,11 +6,11 @@ distro=$1
 architecture=$2
 mirror=${3:-http://http.kali.org/kali}
 
-work_dir=rootfs-$distro-$architecture
+rootfsDir=rootfs-$distro-$architecture
 
 rootfs_chroot() {
     PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' \
-        chroot "$work_dir" "$@"
+        chroot "$rootfsDir" "$@"
 }
 
 
@@ -26,46 +26,46 @@ if [ ! -e /usr/share/keyrings/kali-archive-keyring.gpg ]; then
     exit 1
 fi
 
-rm -rf "$work_dir" "$architecture.$distro.tar.xz"
+rm -rf "$rootfsDir" "$architecture.$distro.tar.xz"
 
 qemu-debootstrap --variant=minbase --components=main,contrib,non-free \
     --arch="$architecture" --include=kali-archive-keyring \
-    "$distro" "$work_dir" "$mirror"
+    "$distro" "$rootfsDir" "$mirror"
 
 rootfs_chroot apt-get -y --no-install-recommends install kali-defaults
 
 rootfs_chroot apt-get clean
 
-cat > "$work_dir/usr/sbin/policy-rc.d" <<-'EOF'
+cat > "$rootfsDir/usr/sbin/policy-rc.d" <<-'EOF'
 	#!/bin/sh
 	exit 101
 EOF
-chmod +x "$work_dir/usr/sbin/policy-rc.d"
+chmod +x "$rootfsDir/usr/sbin/policy-rc.d"
 
-echo 'force-unsafe-io' > "$work_dir"/etc/dpkg/dpkg.cfg.d/docker-apt-speedup
+echo 'force-unsafe-io' > "$rootfsDir"/etc/dpkg/dpkg.cfg.d/docker-apt-speedup
 
 aptGetClean='"rm -f /var/cache/apt/archives/*.deb /var/cache/apt/archives/partial/*.deb /var/cache/apt/*.bin || true";'
-cat > "$work_dir"/etc/apt/apt.conf.d/docker-clean <<-EOF
+cat > "$rootfsDir"/etc/apt/apt.conf.d/docker-clean <<-EOF
 	DPkg::Post-Invoke { ${aptGetClean} };
 	Dir::Cache::pkgcache "";
 	Dir::Cache::srcpkgcache "";
 EOF
 
-echo 'Acquire::Languages "none";' >"$work_dir"/etc/apt/apt.conf.d/docker-no-languages
+echo 'Acquire::Languages "none";' >"$rootfsDir"/etc/apt/apt.conf.d/docker-no-languages
 
-cat > "$work_dir"/etc/apt/apt.conf.d/docker-gzip-indexes <<-'EOF'
+cat > "$rootfsDir"/etc/apt/apt.conf.d/docker-gzip-indexes <<-'EOF'
 	Acquire::GzipIndexes "true";
 	Acquire::CompressionTypes::Order:: "gz";
 EOF
 
-echo 'Apt::AutoRemove::SuggestsImportant "false";' >"$work_dir"/etc/apt/apt.conf.d/docker-autoremove-suggests
+echo 'Apt::AutoRemove::SuggestsImportant "false";' >"$rootfsDir"/etc/apt/apt.conf.d/docker-autoremove-suggests
 
-rm -rf "$work_dir"/var/lib/apt/lists/*
-mkdir -p "$work_dir"/var/lib/apt/lists/partial
-find "$work_dir"/var/log -depth -type f -print0 | xargs -0 truncate -s 0
+rm -rf "$rootfsDir"/var/lib/apt/lists/*
+mkdir -p "$rootfsDir"/var/lib/apt/lists/partial
+find "$rootfsDir"/var/log -depth -type f -print0 | xargs -0 truncate -s 0
 
 echo "Creating ${architecture}.${distro}.tar.xz"
-tar -I 'pixz -1' -C "$work_dir" -pcf "${architecture}.${distro}".tar.xz .
+tar -I 'pixz -1' -C "$rootfsDir" -pcf "${architecture}.${distro}".tar.xz .
 
 if [ -z "$CI_JOB_TOKEN" ]; then
     chmod 775 "${architecture}.${distro}".tar.xz
@@ -73,5 +73,5 @@ if [ -z "$CI_JOB_TOKEN" ]; then
 fi
 
 if [ "$distro" = "kali-last-snapshot" ]; then
-    awk -F= '$1=="VERSION" { print $2 ;}' "$work_dir"/usr/lib/os-release | tr -d '"' > release.version
+    awk -F= '$1=="VERSION" { print $2 ;}' "$rootfsDir"/usr/lib/os-release | tr -d '"' > release.version
 fi
