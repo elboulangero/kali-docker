ce0f68d50fd63352510e6bb54c744b4410241b74
Arnaud Rebillout <arnaudr@kali.org>
2021-03-23 23:05:52 +0700

    docker-push: Style nitpicks

--- docker-push.sh
+++ docker-push.sh
@@ -9,14 +9,14 @@ DOCKER_HUB_REGISTRY_IMAGE="index.docker.io/$DOCKER_HUB_ORGANIZATION"
 
 # Retrieve variables from former docker-build.sh
 # shellcheck source=/dev/null
-. ./"${ARCHITECTURE}"-"${DISTRO}".conf
+. ./"$ARCHITECTURE"-"$DISTRO".conf
 
 if [ -n "$CI_JOB_TOKEN" ]; then
-    docker pull "$CI_REGISTRY_IMAGE"/"${IMAGE:=}":"$VERSION"
+    docker pull "$CI_REGISTRY_IMAGE"/"$IMAGE":"$VERSION"
 
-    docker tag "$CI_REGISTRY_IMAGE"/${IMAGE}:"$VERSION" "$DOCKER_HUB_ORGANIZATION"/${IMAGE}:"$ARCHITECTURE"
-    docker push "$DOCKER_HUB_ORGANIZATION"/${IMAGE}:"$ARCHITECTURE"
-    docker rmi "$CI_REGISTRY_IMAGE"/${IMAGE}:"$VERSION"
+    docker tag "$CI_REGISTRY_IMAGE"/$IMAGE:"$VERSION" "$DOCKER_HUB_ORGANIZATION"/$IMAGE:"$ARCHITECTURE"
+    docker push "$DOCKER_HUB_ORGANIZATION"/$IMAGE:"$ARCHITECTURE"
+    docker rmi "$CI_REGISTRY_IMAGE"/$IMAGE:"$VERSION"
 else
     docker tag "$CI_REGISTRY_IMAGE"/$IMAGE:"$VERSION" "$CI_REGISTRY_IMAGE"/$IMAGE:latest
 fi
@@ -29,8 +29,8 @@ exit
 if [ -n "$CI_JOB_TOKEN" ]; then
     IMAGES=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep "$DOCKER_HUB_ORGANIZATION" | tr '\n' ' ')
     # shellcheck disable=SC2086
-    docker manifest create "$DOCKER_HUB_ORGANIZATION"/${IMAGE}:latest $IMAGES
-    docker manifest push -p "$DOCKER_HUB_REGISTRY_IMAGE"/${IMAGE}:latest
+    docker manifest create "$DOCKER_HUB_ORGANIZATION"/$IMAGE:latest $IMAGES
+    docker manifest push -p "$DOCKER_HUB_REGISTRY_IMAGE"/$IMAGE:latest
     for img in $IMAGES; do
 	docker rmi "$img"
     done
