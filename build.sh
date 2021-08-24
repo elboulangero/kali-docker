2415aa11d5e5f64354a2989a37e55008b0c2636c
Arnaud Rebillout <arnaudr@kali.org>
2021-08-24 22:41:53 +0700

    build: Rework usage a bit so that it's easy to build any combination, or all

--- build.sh
+++ build.sh
@@ -3,16 +3,18 @@
 set -e
 set -u
 
-DISTROS="${*:-kali-rolling}"
-EXTRA_DISTROS=""
-ARCHS="amd64"
+DISTROS="${1:-kali-rolling}"
+EXTRA_DISTROS="${2:-}"
+ARCHS="${3:-amd64}"
 
-#DISTROS="kali-rolling kali-dev kali-last-snapshot"
-#EXTRA_DISTROS="kali-experimental kali-bleeding-edge"
-#ARCHS="amd64 arm64 armhf"
+[ "$DISTROS" == all ] && DISTROS="kali-rolling kali-dev kali-last-snapshot"
+[ "$EXTRA_DISTROS" == all ] && EXTRA_DISTROS="kali-experimental kali-bleeding-edge"
+[ "$ARCHS" == all ] && ARCHS="amd64 arm64 armhf"
 
 echo "Distributions: $DISTROS"
+echo "Extra distros: $EXTRA_DISTROS"
 echo "Architectures: $ARCHS"
+
 for distro in $DISTROS; do
     for arch in $ARCHS; do
         echo "========================================"
@@ -25,6 +27,7 @@ for distro in $DISTROS; do
     done
     sudo ./docker-push-manifest.sh "$distro" "$arch"
 done
+
 for distro in $EXTRA_DISTROS; do
     for arch in $ARCHS; do
         echo "========================================"
